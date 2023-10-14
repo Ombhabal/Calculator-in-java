82af696fcd5bfa18bcb1d7f13e983579c2a57988
Ombhabal <143935443+Ombhabal@users.noreply.github.com>
2023-10-14 20:28:34 +0530

    Add files via upload

---
+++ src/main/java/com/OmBhabal/calculator/App.java
@@ -0,0 +1,10 @@
+package com.OmBhabal.calculator;
+
+import com.OmBhabal.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
